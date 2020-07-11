b9c5dc24c7026af42ea0a2a38c9b94c56a065429
LuongDzai <vuxuanluong12345@gmail.com>
2020-07-12 00:07:00 +0700

    create firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Your web app's Firebase configuration
+var config = {
+  apiKey: "AIzaSyDZrkBWCP_lT7fypKEoQgeHH0TkPF8RUK0",
+  authDomain: "mrgrey-marioplan.firebaseapp.com",
+  databaseURL: "https://mrgrey-marioplan.firebaseio.com",
+  projectId: "mrgrey-marioplan",
+  storageBucket: "mrgrey-marioplan.appspot.com",
+  messagingSenderId: "595859895188",
+  appId: "1:595859895188:web:156e681ebee676feb11581",
+  measurementId: "G-WFD6RRNSQV",
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
