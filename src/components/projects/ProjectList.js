1eba223f691521586fbc26b34409b0c561583f16
LuongDzai <vuxuanluong12345@gmail.com>
2020-07-11 21:36:17 +0700

    adding dummy data

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ProjectSummary from "./ProjectSummary";
 
-function ProjectList() {
+function ProjectList({projects}) {
   return (
     <div>
       <div className="project-list section">
-        <ProjectSummary />
-        <ProjectSummary />
-        <ProjectSummary />
-        <ProjectSummary />
+        {projects.map((project)=>{
+          return(
+            <ProjectSummary project={project} key={project.id}/>
+          )
+        })}
       </div>
     </div>
   );
