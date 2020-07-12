3ba08a853c9e003424ffe4497c9a655b292f3bf9
LuongDzai <vuxuanluong12345@gmail.com>
2020-07-12 13:37:31 +0700

    firebase auth with redux

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
+import { connect } from "react-redux";
 
-export default function Navbar() {
+function Navbar() {
   return (
     <div>
       <nav className="nav-wrapper grey darken-3">
@@ -18,3 +19,12 @@ export default function Navbar() {
     </div>
   );
 }
+
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps)(Navbar)
